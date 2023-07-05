169e0505e2922e0df2717109e35a98a8c0bdb3ff
Humeyra Uçar <66906545+humeyraucar@users.noreply.github.com>
2023-07-05 16:49:37 +0300

    Update app-response.py

--- app-response.py
+++ app-response.py
@@ -5,11 +5,6 @@ import json
 
 app = Flask(__name__)
 
-
-#@app.route("/")
-#def hello_world():
-#    return "<p>Hello, World!</p>"
-    
 @app.route("/slackbutton", methods=['POST'])
 def respond():
     """
